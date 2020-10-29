b6d1fbe184e008de0967407ba9616a90c4aa119c
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-10-29 21:42:52 +0530

    adding

---
+++ payment-atpsoda/src/test/java/io/helidon/examples/sockshop/payment/mongo/MongoPaymentResourceIT.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+package io.helidon.examples.sockshop.payment.mongo;
+
+import io.helidon.examples.sockshop.payment.PaymentResourceIT;
+
+/**
+ * Integration tests for {@link io.helidon.examples.sockshop.payment.PaymentResource},
+ * using MongoDB for persistence.
+ */
+class MongoPaymentResourceIT extends PaymentResourceIT {
+}
